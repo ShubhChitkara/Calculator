391fe52e019126827e5eb95210446146eb27307c
ShubhChitkara <186718633+ShubhChitkara@users.noreply.github.com.>
2025-01-12 15:22:46 +0000

    Added some functions

---
+++ server.js
@@ -0,0 +1,20 @@
+function add(a, b) {
+  let ans = a + b;
+  return ans;
+  
+};
+
+function subtract(c, d) {
+  let ans = c - d;
+  return ans;
+};
+
+function multiply(e, f) {
+  const ans = e * f;
+  return ans;
+};
+
+function divide(g, h) {
+  const ans = g / h;
+  return ans;
+};
