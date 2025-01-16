1904c70b9c49edb4409f4da9dd138f46d66e4011
ShubhChitkara <186718633+ShubhChitkara@users.noreply.github.com.>
2025-01-16 18:03:29 +0000

    Worked on the visual part of the calc

--- server.js
+++ server.js
@@ -1,3 +1,20 @@
+const clear=document.querySelector(".clear");
+const one=document.querySelector(".one");
+const two=document.querySelector(".two");
+const three=document.querySelector(".three");
+const four=document.querySelector(".four");
+const five=document.querySelector(".five");
+const six=document.querySelector(".six");
+const seven=document.querySelector(".seven");
+const eight=document.querySelector(".eight");
+const nine=document.querySelector(".nine");
+const zero=document.querySelector(".zero");
+const division=document.querySelector(".divide");
+const multiple=document.querySelector(".multiply");
+const minus=document.querySelector(".minus");
+const plus=document.querySelector(".plus");
+const dot=document.querySelector(".dot");
+const equals=document.querySelector(".equalsto");
 function add(a, b) {
   let ans = a + b;
   return ans;
@@ -18,3 +35,42 @@ function divide(g, h) {
   const ans = g / h;
   return ans;
 };
+const num1="";
+const operator="";
+const num2="";
+function operate(num1,operator,num2){
+  if(operator="+"){
+    add(num1,num2);
+  }else if(operator="-"){
+    subtract(num1,num2);
+  }else if(operator="*"){
+    multiply(num1,num2);
+  }else{
+    divide(num1,num2);
+  }
+}
+const display=document.querySelector(".text");
+display.textContent=[];
+clear.addEventListener("click",display=[]);
+one.addEventListener("click",display.textContent+=[1]);
+two.addEventListener("click",display.textContent+=[2]);
+three.addEventListener("click",display.textContent+=[3]);
+four.addEventListener("click",display.textContent+=[4]);
+five.addEventListener("click",display.textContent+=[5]);
+six.addEventListener("click",display.textContent+=[6]);
+seven.addEventListener("click",display.textContent+=[7]);
+eight.addEventListener("click",display.textContent+=[8]);
+nine.addEventListener("click",display.textContent+=[9]);
+zero.addEventListener("click",display.textContent+=[0]);
+division.addEventListener("click",display.textContent+=["/"]);
+multiple.addEventListener("click",display.textContent+=["*"]);
+minus.addEventListener("click",display.textContent+=["-"]);
+plus.addEventListener("click",display.textContent+=["+"]);
+dot.addEventListener("click",display.textContent+=["."]);
+equals.addEventListener("click",solution());
+console.log(display)
+
+
+function solution(display){
+  
+}
